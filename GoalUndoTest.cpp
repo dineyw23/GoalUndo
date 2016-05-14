b2330cb6719537be3ada92a1ce660e916c3de1a1
dineyw23 <diney.12323@gmail.com>
2016-05-13 20:51:37 -0700

    Adds two tests for getGoal function

--- GoalUndoTest.cpp
+++ GoalUndoTest.cpp
@@ -18,3 +18,30 @@ TEST(GoalUndoTest, sanityCheck)
 {
 	ASSERT_TRUE(true);
 }
+
+/*
+ *  Test getGoal function to return empty string
+ *  when there are no goals.
+ */
+
+TEST(GoalUndoTest, getEmptyGoal)
+{
+  std::string empty = "";
+  GoalUndo goalUndoObj;
+  ASSERT_EQ(empty,goalUndoObj.getGoal());
+}
+
+/*
+ *  Test getGoal to check if it return proper latest goal.
+ */
+
+TEST(GoalUndoTest, getLatestGoal)
+{
+  GoalUndo goalUndoObj;
+  goalUndoObj.addOperation("Eat Healthy","proteins");
+  goalUndoObj.addOperation("Sleep Well","10pm");
+  ASSERT_NE("Eat Healthy",goalUndoObj.getGoal());
+  ASSERT_EQ("Sleep Well",goalUndoObj.getGoal());
+}
+
+
