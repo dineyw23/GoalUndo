d39f50768c310e11e52d792e458741e84785fdca
dineyw23 <diney.12323@gmail.com>
2016-05-14 02:10:19 -0700

    Adds tests for addOperation functions

--- GoalUndoTest.cpp
+++ GoalUndoTest.cpp
@@ -20,6 +20,87 @@ TEST(GoalUndoTest, sanityCheck)
 }
 
 /*
+ *  addOperations without any goal to test if operation if copied 
+ *  to goal correctly.   
+ *
+ */
+
+TEST(GoalUndoTest, addNewOpOnlyWithoutGoal)
+{
+  GoalUndo goalUndoObj;
+  goalUndoObj.addOperation("Trial");
+  goalUndoObj.addOperation("TrialOperation 2");
+  EXPECT_EQ("Trial",goalUndoObj.getGoal());
+  ASSERT_EQ("Trial TrialOperation 2",goalUndoObj.getOperations()); 
+}
+
+/*
+ * Test to check if addOperation functions adds goal and 
+ * operations properly.
+ */
+
+TEST(GoalUndoTest, addOpWithGoal)
+{
+  GoalUndo goalUndoObj;
+  goalUndoObj.addOperation("Eat Healthy","proteins");
+  goalUndoObj.addOperation("carbs");
+  EXPECT_EQ("Eat Healthy",goalUndoObj.getGoal());
+  ASSERT_EQ("proteins carbs",goalUndoObj.getOperations());
+}
+
+/*
+ * Add empty string to operations which should be copied to  
+ * Goal.
+ */
+
+TEST(GoalUndoTest, addNewOpWithoutGoalEmpty)
+{
+  GoalUndo goalUndoObj;
+  goalUndoObj.addOperation("");
+  ASSERT_EQ("",goalUndoObj.getGoal());
+  ASSERT_EQ("",goalUndoObj.getOperations());
+}
+
+/*
+ *  Add empty string to addOperation(newGoal,newOp) function.
+ *  If both arg are empty.
+ */
+
+TEST(GoalUndoTest, addNewGoalandOpEmpty)
+{
+  GoalUndo goalUndoObj;
+  std::string empty = "";
+  goalUndoObj.addOperation(empty,empty);
+  ASSERT_EQ(empty,goalUndoObj.getGoal());
+  ASSERT_EQ(empty,goalUndoObj.getOperations());
+  goalUndoObj.addOperation("only goal",empty);
+  goalUndoObj.addOperation(empty,"only operation");
+}
+
+/* 
+ * Test where first argument is empty and second is valid.
+ */
+
+TEST(GoalUndoTest,addEmptyGoalValidOp)
+{
+  GoalUndo goalUndoObj;
+  std::string empty = "";
+  goalUndoObj.addOperation("","Valid operation");
+  ASSERT_EQ(empty,goalUndoObj.getGoal());
+}
+
+/*
+ * Test where first argument is valid and second is empty.
+ */
+
+TEST(GoalUndoTest,addValidGoalEmptyOp)
+{
+  GoalUndo goalUndoObj;
+  std::string empty = "";
+  goalUndoObj.addOperation("Valid Goal",empty);
+  ASSERT_EQ(empty,goalUndoObj.getGoal());
+}
+/*
  *  Test getGoal function to return empty string
  *  when there are no goals.
  */
